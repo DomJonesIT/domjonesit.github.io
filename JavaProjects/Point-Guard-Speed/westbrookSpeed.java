44d8e087f65dd5816125cca753037db038776bad
DomJonesIT <DomJonesIT@gmail.com>
2016-04-08 14:34:42 -0400

    westbrookSpeed.java
    Russell Westbrook speed class object

---
+++ JavaProjects/Point-Guard-Speed/westbrookSpeed.java
@@ -0,0 +1,10 @@
+package com.domjonesit;
+
+public class westbrookSpeed extends courtSpeed {
+    String method = "wearing Air Jordan XXX";
+    
+    public void seconds() {
+        System.out.println("Running baseline to baseline " + method);
+        
+    }
+}
