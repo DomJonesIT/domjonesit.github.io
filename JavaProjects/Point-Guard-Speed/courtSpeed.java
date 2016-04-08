17c0178a83c59ee1de28da900206d5f9fb6826ec
DomJonesIT <DomJonesIT@gmail.com>
2016-04-08 14:26:37 -0400

    courtSpeed.java
    Court speed

---
+++ JavaProjects/Point-Guard-Speed/courtSpeed.java
@@ -0,0 +1,13 @@
+package com.domjonesit;
+
+/**
+ *
+ * @author DJones
+ */
+public class courtSpeed {
+    int speed;
+    
+    public void displaySpeed() {
+        System.out.println("Speed: " + speed);
+    }
+}
