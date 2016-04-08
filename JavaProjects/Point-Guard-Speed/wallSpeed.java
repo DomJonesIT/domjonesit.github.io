82fd2be3bfd2f4011a1252e5b5e4237d94c8feb1
DomJonesIT <DomJonesIT@gmail.com>
2016-04-08 14:32:29 -0400

    wallSpeed.java
    John Wall speed object

---
+++ JavaProjects/Point-Guard-Speed/wallSpeed.java
@@ -0,0 +1,10 @@
+package com.java24hours;
+
+public class wallSpeed extends courtSpeed {
+    String method = "wearing Nike Kyrie II";
+    
+    public void seconds() {
+        System.out.println("Running baseline to baseline " + method);
+        
+    }
+}
