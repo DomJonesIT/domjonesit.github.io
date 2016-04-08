7830c39854c558522a4d217894c4c0085f5d7c6b
DomJonesIT <DomJonesIT@gmail.com>
2016-04-08 14:08:47 -0400

    Alphabet Program
    Allows user to type in letter and displays coordinating word starting with that letter.

---
+++ C-Projects/AlphabetProgram/main.cpp
@@ -0,0 +1,78 @@
+//
+//  main.cpp
+//  Dominique Assignment 4
+//
+//  Created by Dominique Jones on 2/8/15.
+//  Copyright (c) 2015 Dominique L. Jones. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char letter;
+    
+    cout << "Enter the first letter of the IACO alphabet"
+    << "word you need: " << endl;
+    
+    
+    cin >> letter;
+    
+    if (letter == 'A')
+        cout << "Alpha" << endl;
+    if (letter == 'B')
+        cout << "Bravo" << endl;
+    if (letter == 'C')
+        cout << "Charlie" << endl;
+    if (letter == 'D')
+        cout << "Delta" << endl;
+    if (letter == 'E')
+        cout << "Echo" << endl;
+    if (letter == 'F')
+        cout << "Foxtrot" << endl;
+    if (letter == 'G')
+        cout << "Golf" << endl;
+    if (letter == 'H')
+        cout << "Hotel" << endl;
+    if (letter == 'I')
+        cout << "India" << endl;
+    if (letter == 'J')
+        cout << "Juliet" << endl;
+    if (letter == 'K')
+        cout << "Kilo" << endl;
+    if (letter == 'L')
+        cout << "Lima" << endl;
+    if (letter == 'M')
+        cout << "Mike" << endl;
+    if (letter == 'N')
+        cout << "November" << endl;
+    if (letter == 'O')
+        cout << "Oscar" << endl;
+    if (letter == 'P')
+        cout << "Papa" << endl;
+    if (letter == 'Q')
+        cout << "Quebec" << endl;
+    if (letter == 'R')
+        cout << "Romeo" << endl;
+    if (letter == 'S')
+        cout << "Sierra" << endl;
+    if (letter == 'T')
+        cout << "Tango" << endl;
+    if (letter == 'U')
+        cout << "Uniform" << endl;
+    if (letter == 'V')
+        cout << "Victor" << endl;
+    if (letter == 'W')
+        cout << "Whiskey" << endl;
+    if (letter == 'X')
+        cout << "X-Ray" << endl;
+    if (letter == 'Y')
+        cout << "Yankee" << endl;
+    if (letter == 'Z')
+        cout << "Zulu" << endl;
+    
+    return 0;
+}
